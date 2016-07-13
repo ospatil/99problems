7c149109f266c5f0c56e82849f574ba9c655d083
Omkar Patil <omkar.patil@fisglobal.com>
2016-07-12 21:42:46 -0400

    Add foldl

--- lib/00_recursion_practice.exs
+++ lib/00_recursion_practice.exs
@@ -1,7 +1,11 @@
 defmodule Recur do
-  # map
+  # map. builtin - Enum.map/2
   def map([], _func), do: []
   def map([head | tail], func) when is_function(func, 1), do: [func.(head) | map(tail, func)]
+
+  # left fold. builtin - List.foldl/3
+  def foldl([], acc, _func), do: acc
+  def foldl([head | tail], acc, func) when is_function(func, 2), do: foldl(tail, func.(head, acc), func)
 end
 
 ExUnit.start
@@ -11,5 +15,9 @@ defmodule RecurTest do
 
   test "Recur.map" do
     assert Recur.map([1,2,3], &(&1 * 2)) == [2,4,6]
+  end
+
+  test "Recur.foldl" do
+    assert Recur.foldl([1,2,3], 0, &(&1 + &2)) == 6
   end
 end
