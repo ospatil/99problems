4a5d3a3983ea07109f84870d82ccfd7d967858da
Omkar Patil <ospatil@gmail.com>
2016-07-27 21:29:22 -0400

    Implement Enum functions

--- lib/00_recursion_practice.exs
+++ lib/00_recursion_practice.exs
@@ -1,4 +1,18 @@
-defmodule Recur do
+# Implement builtin Enum and List module api but only for lists
+
+defmodule MyList do
+  # Enum.all?/2, Enum.all?/1
+  def all?(list, fun \\ &(&1))
+  def all?(list, fun), do: do_all?(list, fun, true)
+  defp do_all?([], _, bool), do: bool
+  defp do_all?([head | tail], fun, true), do: do_all?(tail, fun, fun.(head))
+  defp do_all?([_ | tail], fun, false), do: do_all?(tail, fun, false)
+
+  # Enum.reverse/1
+  def reverse(list), do: do_reverse(list, [])
+  defp do_reverse([], result), do: result
+  defp do_reverse([head | tail], result), do: do_reverse(tail, [head | result])
+
   # map. builtin - Enum.map/2
   def map([], _func), do: []
   def map([head | tail], func) when is_function(func, 1), do: [func.(head) | map(tail, func)]
@@ -10,14 +24,38 @@ end
 
 ExUnit.start
 
-defmodule RecurTest do
-  	, async: true
+defmodule MyListTest do
+  use ExUnit.Case, async: true
 
-  test "Recur.map" do
-    assert Recur.map([1,2,3], &(&1 * 2)) == [2,4,6]
+  test "all? should return true for an empty list" do
+    assert MyList.all?([]) == true
   end
 
-  test "Recur.foldl" do
-    assert Recur.foldl([1,2,3], 0, &(&1 + &2)) == 6
+  test "all? should return false if any value in the list is false" do
+    assert MyList.all?([true, false, true]) == false
+  end
+
+  test "all? should use provided function" do
+    assert MyList.all?([1, 2, 3, 0], &(&1 > 0)) == false
+  end
+
+  test "reverse should return empty list for empty list" do
+    assert MyList.reverse([]) == []
+  end
+
+  test "reverse should return reversed list" do
+    assert MyList.reverse([1, 2, 3]) == [3, 2, 1]
+  end
+
+  test "map should return empty list for empty list" do
+    assert MyList.map([], &(&1)) == []
+  end
+
+  test "map should return mapped list" do
+    assert MyList.map([1, 2, 3], &(&1 + 1)) == [2, 3, 4]
+  end
+
+  test "MyList.foldl" do
+    assert MyList.foldl([1,2,3], 0, &(&1 + &2)) == 6
   end
 end
